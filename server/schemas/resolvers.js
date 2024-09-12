5dc1a8122a6a2bda032566ad3415ffaceda187a9
Chris Moore <chrismoorelmt@gmail.com>
2024-09-11 22:08:36 -0500

    Created utils dir and coded auth.js, dateFormat.js, built schemas, created models files

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,12 +1,84 @@
-const {Profile} = require('../models');
+const {User, Recording, Profile} = require('../models');
+const {signToken, AuthenticationError} = require('../utils/auth');
 
 const resolvers = {
     Query: {
-
+        users: async () => {
+            return User.find().populate('recordings');
+        },
+        user: async (parent, { username }) => {
+            return User.findOne({ username }).populate('recordings');
+        },
+        recordings: async (parent, { username }) => {
+            const params = username ? { username } : {};
+            return Recording.find(params).sort({ createdAt: -1 });
+        },
+        recording: async (parent, { recordingId }) => {
+            return Recording.findOne({ _id: recordingId });
+        },
+        me: async (parent, args, context) => {
+            if (context.user) {
+              return User.findOne({ _id: context.user._id }).populate('recordings');
+            }
+            throw AuthenticationError;
+        },
     },
 
     Mutation: {
-
+        addUser: async (parent, { username, email, password }) => {
+            const user = await User.create({ username, email, password });
+            const token = signToken(user);
+            return { token, user };
+        },
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+      
+            if (!user) {
+              throw AuthenticationError;
+            }
+      
+            const correctPw = await user.isCorrectPassword(password);
+      
+            if (!correctPw) {
+              throw AuthenticationError;
+            }
+      
+            const token = signToken(user);
+      
+            return { token, user };
+        },
+        addRecording: async (parent, { recordingAudio }, context) => {
+            if (context.user) {
+              const recording = await Recording.create({
+                recordingAudio,
+                recordingAuthor: context.user.username,
+              });
+      
+              await User.findOneAndUpdate(
+                { _id: context.user._id },
+                { $addToSet: { recordings: recording._id } }
+              );
+      
+              return recording;
+            }
+            throw AuthenticationError;
+        },
+        removeRecording: async (parent, { recordingId }, context) => {
+            if (context.user) {
+              const recording = await Recording.findOneAndDelete({
+                _id: recordingId,
+                recordingAuthor: context.user.username,
+              });
+      
+              await User.findOneAndUpdate(
+                { _id: context.user._id },
+                { $pull: { recordings: recording._id } }
+              );
+      
+              return recording;
+            }
+            throw AuthenticationError;
+        },
     },
 }
 
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,4 +1,40 @@
 const typeDefs = `
+  type User {
+    _id: ID
+    username: String!
+    email: String
+    recordings: [Recording]
+  }
+
+  type Recording { 
+    _id: ID
+    title: String
+    author: String
+    createdAt: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    users: [User]
+    user(username: String!): User
+    recordings(username: String): [Recording]
+    recording(recordingId: ID!): Recording
+    me: User
+  }
+
+  type Mutation {
+    addUser(username: String!, email: String!): Auth
+    login(email: String!): Auth
+    addRecording(recordingAudio): Recording
+    removeRecording(recordingId: ID!): Recording
+  }
 `;
+//? What parameters does Recording require besides title, author, and createdAt? What parameter will make up the content of stored recording?
+//* For now, using recordingAudio as a placeholder/possible solution
+//TODO: addRecording requires content parameters. example: A mutation adding a blog post would require addPost(postText: String!).
 
 module.exports = typeDefs;
