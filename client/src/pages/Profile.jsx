1014436b49c87f5455c3496b0d8d316b2d7399d4
Khoi Mai <thegreatwall88@gmail.com>
2024-09-12 14:25:07 -0500

    login and signup working

---
+++ client/src/pages/Profile.jsx
@@ -0,0 +1,13 @@
+import { Container, Typography } from '@mui/material';
+
+const ProfilePage = () => {
+  return (
+    <Container>
+      <Typography variant="h4" component="h1" align="center" sx={{ mt: 4 }}>
+        This is the Profile page
+      </Typography>
+    </Container>
+  );
+};
+
+export default ProfilePage;
--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,85 +1,77 @@
-const {User, Recording, Profile} = require('../models');
-const {signToken, AuthenticationError} = require('../utils/auth');
+const { User, Recording } = require('../models');
+const { signToken, AuthenticationError } = require('../utils/auth');
 
 const resolvers = {
-    Query: {
-        users: async () => {
-            return User.find().populate('recordings');
-        },
-        user: async (parent, { username }) => {
-            return User.findOne({ username }).populate('recordings');
-        },
-        recordings: async (parent, { username }) => {
-            const params = username ? { username } : {};
-            return Recording.find(params).sort({ createdAt: -1 });
-        },
-        recording: async (parent, { recordingId }) => {
-            return Recording.findOne({ _id: recordingId });
-        },
-        me: async (parent, args, context) => {
-            if (context.user) {
-              return User.findOne({ _id: context.user._id }).populate('recordings');
-            }
-            throw AuthenticationError;
-        },
+  Query: {
+    users: async () => {
+      return User.find().populate('recordings');
     },
+    user: async (parent, { username }) => {
+      return User.findOne({ username }).populate('recordings');
+    },
+    recordings: async (parent, { username }) => {
+      const params = username ? { username } : {};
+      return Recording.find(params).sort({ createdAt: -1 });
+    },
+    recording: async (parent, { recordingId }) => {
+      return Recording.findOne({ _id: recordingId });
+    },
+    me: async (parent, args, context) => {
+      if (context.user) {
+        return User.findOne({ _id: context.user._id }).populate('recordings');
+      }
+      throw new AuthenticationError('Not authenticated');
+    },
+  },
 
-    Mutation: {
-        addUser: async (parent, { username, email, password }) => {
-            const user = await User.create({ username, email, password });
-            const token = signToken(user);
-            return { token, user };
-        },
-        login: async (parent, { email, password }) => {
-            const user = await User.findOne({ email });
-      
-            if (!user) {
-              throw AuthenticationError;
-            }
-      
-            const correctPw = await user.isCorrectPassword(password);
-      
-            if (!correctPw) {
-              throw AuthenticationError;
-            }
-      
-            const token = signToken(user);
-      
-            return { token, user };
-        },
-        addRecording: async (parent, { recordingAudio }, context) => {
-            if (context.user) {
-              const recording = await Recording.create({
-                recordingAudio,
-                recordingAuthor: context.user.username,
-              });
-      
-              await User.findOneAndUpdate(
-                { _id: context.user._id },
-                { $addToSet: { recordings: recording._id } }
-              );
-      
-              return recording;
-            }
-            throw AuthenticationError;
-        },
-        removeRecording: async (parent, { recordingId }, context) => {
-            if (context.user) {
-              const recording = await Recording.findOneAndDelete({
-                _id: recordingId,
-                recordingAuthor: context.user.username,
-              });
-      
-              await User.findOneAndUpdate(
-                { _id: context.user._id },
-                { $pull: { recordings: recording._id } }
-              );
-      
-              return recording;
-            }
-            throw AuthenticationError;
-        },
+  Mutation: {
+    addUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
+      const token = signToken(user);
+      return { token, user };
     },
-}
+    login: async (parent, { username, password }) => {
+      const user = await User.findOne({ username });
+      if (!user) {
+        throw new AuthenticationError('No user found with this username');
+      }
+      const correctPw = await user.isCorrectPassword(password);
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect password');
+      }
+      const token = signToken(user);
+      return { token, user };
+    },
+    addRecording: async (parent, { title, author, recordingAudio }, context) => {
+      if (context.user) {
+        const recording = await Recording.create({
+          title,
+          author,
+          recordingAudio
+        });
+        await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $addToSet: { recordings: recording._id } }
+        );
+        return recording;
+      }
+      throw new AuthenticationError('Not authenticated');
+    },
+    removeRecording: async (parent, { recordingId }, context) => {
+      if (context.user) {
+        const recording = await Recording.findOneAndDelete({
+          _id: recordingId,
+          author: context.user.username,
+        });
+        await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $pull: { recordings: recording._id } }
+        );
+        return recording;
+      }
+      throw new AuthenticationError('Not authenticated');
+    },
+  },
+};
 
 module.exports = resolvers;
