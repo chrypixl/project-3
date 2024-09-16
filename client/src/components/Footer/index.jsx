2dd10fa74785c60aad3d16409b8ed671d89c0a17
Khoi Mai <thegreatwall88@gmail.com>
2024-09-16 17:54:20 -0500

    tipjar adjustment

--- client/src/components/Footer/index.jsx
+++ client/src/components/Footer/index.jsx
@@ -3,12 +3,10 @@ import Tipjar from '../Tipjar';
 const Footer = () => {
   return (
     <footer className="w-100 mt-auto bg-secondary p-4" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-      {/* Align Tipjar to the left */}
       <div style={{ flex: 1, display: 'flex', justifyContent: 'flex-start' }}>
         <Tipjar />
       </div>
 
-      {/* Center the text */}
       <div style={{ flex: 1, textAlign: 'center' }} className="container mb-5">
         <h4>
           Made for the{' '}
@@ -23,8 +21,6 @@ const Footer = () => {
           of sick beats.
         </h4>
       </div>
-
-      {/* Empty div to balance the layout on the right */}
       <div style={{ flex: 1 }}></div>
     </footer>
   );
